2d2dd8e0cf5e41ab8420a6e5dcdcc4f437ff09f4
Eden Jia <edenjia@hotmail.com>
2019-02-09 23:31:47 +0000

    Added ability for front end to request the types and also nearby locations of type within specific radius of the user's GPS

--- NODEJS/node_modules/googleplaces/test/config.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    "use strict";
-
-    exports.apiKey = process.env.GOOGLE_PLACES_API_KEY || "your google api key (server) comes here";
-    exports.outputFormat = process.env.GOOGLE_PLACES_OUTPUT_FORMAT || "json";
-
-})();
